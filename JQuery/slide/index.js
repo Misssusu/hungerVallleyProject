19d0acdca4120693ffb906100465cc39a44a3b5e
suyuan <suyuan326@qq.com>
2019-06-26 14:48:53 +0800

    优化无缝轮播

--- JQuery/slide/index.js
+++ JQuery/slide/index.js
@@ -1,31 +1,36 @@
-$('.box img:nth-child(1)').addClass('current');
-$('.box img:nth-child(2)').addClass('enter');
-$('.box img:nth-child(3)').addClass('enter');
-setTimeout(() =>{
-    $('.box img:nth-child(1)').removeClass('current').addClass('leave')
+let n;
+init();
+setInterval(() =>{
+    makeLeave(imgNode(n))
     .one('transitionend',(evt) => {
-        $(evt.currentTarget).removeClass('leave').addClass('enter');
+        makeEnter($(evt.currentTarget));
     })
-    $('.box img:nth-child(2)').removeClass('enter').addClass('current');
+    makeCurrent(imgNode(n+1));
+    n+=1;
 },3000)
-setTimeout(() =>{
-    $('.box img:nth-child(2)').removeClass('current').addClass('leave')
-    .one('transitionend',(evt) => {
-        $(evt.currentTarget).removeClass('leave').addClass('enter');
-    })
-    $('.box img:nth-child(3)').removeClass('enter').addClass('current');
-},6000)
-setTimeout(() =>{
-    $('.box img:nth-child(3)').removeClass('current').addClass('leave')
-    .one('transitionend',(evt) => {
-        $(evt.currentTarget).removeClass('leave').addClass('enter');
-    })
-    $('.box img:nth-child(1)').removeClass('enter').addClass('current');
-},9000)
-setTimeout(() =>{
-    $('.box img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend',(evt) => {
-        $(evt.currentTarget).removeClass('leave').addClass('enter');
-    })
-    $('.box img:nth-child(2)').removeClass('enter').addClass('current');
-},12000)
+function imgNodeIndex(n) {
+    if(n > 3) {
+        n = n%3;
+        if(n === 0) {
+            n = 3;
+        }
+    }
+    return n;
+}
+function init() {
+    n = 1;
+    imgNode(n).addClass('current')
+    .siblings().addClass('enter')
+}
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current');
+}
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave');
+}
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter');
+}
+function imgNode(n) {
+    return $(`.box img:nth-child(${imgNodeIndex(n)})`);
+}
